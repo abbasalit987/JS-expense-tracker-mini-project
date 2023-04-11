9184a128389b0b6b778ebfa4c4554fd120626818
abbasalit987 <abbasalit987@gmail.com>
2023-04-11 18:01:19 +0530

    commit 5/removing post function in edit functionality

--- frontend/main.js
+++ frontend/main.js
@@ -3,9 +3,9 @@ let loginForm = document.getElementById("expenses");
 window.addEventListener("DOMContentLoaded", () => {
     axios.get("http:localhost:5000/get-expenses")
         .then((resp) => {
-            console.log(resp);
+            //console.log(resp);
             for (let i =0; i<resp.data.allExpenses.length; i++){
-                addUser(resp.data.allExpenses[i]);
+                addExpense(resp.data.allExpenses[i]);
             }
         })
         .catch((err) => console.log(err))
@@ -16,9 +16,6 @@ function save(event) {
     event.preventDefault();
 
     expended_on = event.target.expendedon.value;
-    //lastName = event.target.field2.value;
-
-    //fullName = firstName + ' ' + lastName;
     console.log(expended_on);
     amount_spent = event.target.amount.value;
     var x = document.getElementById("category");
@@ -26,41 +23,62 @@ function save(event) {
     category_ = event.target.category.value = x.options[i].text;
 
     let expense_details = {
-        expense: expended_on,
+        expended_on: expended_on,
         amount : amount_spent,
         category : category_
     };
 
     expense_details_serialized = JSON.stringify(expense_details);
-    console.log(expense_details_serialized);
-
-    localStorage.setItem(expended_on,expense_details_serialized);
-    addExpense(expense_details);
+    axios.post("http://localhost:5000/add-expense", expense_details)
+    .then((response) => {
+        //console.log(response);
+        addExpense(response.data.newExpenseDetail);
+    })
+    .catch((err) => {
+        console.log(err);
+    })
   }
 
   function addExpense(expense_details) {
     
     const parentElem = document.getElementById('expenses_list');
     const childElem = document.createElement('li');
-    childElem.textContent = expense_details.expense + ' - ' + expense_details.amount + ' - ' + expense_details.category;
+    childElem.textContent = expense_details.expended_on + ' - ' + expense_details.amount + ' - ' + expense_details.category;
 
     const deleteBtn = document.createElement('input');
     deleteBtn.type = 'button'
     deleteBtn.value = 'Delete'
     deleteBtn.onclick = () => {
-        localStorage.removeItem(expense_details.expense);
-        parentElem.removeChild(childElem);
+        axios.delete(`http://localhost:5000/delete-expense/${expense_details.id}`)
+            .then(() => {
+                parentElem.removeChild(childElem);
+            })
+            .catch((err) => {
+                console.log(err);
+            })
     }
     const editBtn = document.createElement('input');
     editBtn.type = 'button'
     editBtn.value = 'Edit'
     editBtn.onclick = () => {
-        //document.getElementById('firstName').value = user_details.name.split(' ')[0];
-        document.getElementById('expendedon').value = expense_details.expense;
-        document.getElementById('amount').value = expense_details.amount;
-        document.getElementById('category').value = expense_details.category;
-        localStorage.removeItem(expense_details.expense);
-        parentElem.removeChild(childElem);
+        axios.delete(`http://localhost:5000/delete-expense/${expense_details.id}`)
+            .then((response) => {
+                document.getElementById('expendedon').value = expense_details.expended_on;
+                document.getElementById('amount').value = expense_details.amount;
+                document.getElementById('category').value = expense_details.category;
+                parentElem.removeChild(childElem);
+                // axios.post("http://localhost:5000/add-expense", expense_details)
+                //     .then((response) => {
+                //         //console.log(response);
+                //         addExpense(response.data.newExpenseDetail);
+                //     })
+                //     .catch((err) => {
+                //         console.log(err);
+                //     })
+            })
+            .catch((err) => {
+                console.log(err);
+            })
     }
     childElem.appendChild(editBtn);
     childElem.appendChild(deleteBtn);
